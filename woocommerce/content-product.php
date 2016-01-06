e03a739b65015f9dd672d49ab6fd1b2db2c43006
IvanRyzhik <ryzhik@fruitfulcode.com>
2016-01-06 11:09:18 +0200

    Woocommerce templates update
    - content-product
    - content-product_cat
    - variable

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -4,52 +4,45 @@
  *
  * Override this template by copying it to yourtheme/woocommerce/content-product.php
  *
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     2.4.0
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 2.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
-global $product, $woocommerce_loop, $anaglyph_config, $time_post_delay;
+global $product, $woocommerce_loop;
 
 // Store loop count we're currently on
-if ( empty( $woocommerce_loop['loop'] ) )
+if ( empty( $woocommerce_loop['loop'] ) ) {
 	$woocommerce_loop['loop'] = 0;
+}
 
 // Store column count for displaying the grid
-if ( empty( $woocommerce_loop['columns'] ) )
-	 $woocommerce_loop['columns'] = apply_filters( 'loop_shop_columns', 4 );
+if ( empty( $woocommerce_loop['columns'] ) ) {
+	$woocommerce_loop['columns'] = apply_filters( 'loop_shop_columns', 4 );
+}
 
 // Ensure visibility
-if ( ! $product || ! $product->is_visible() )
+if ( ! $product || ! $product->is_visible() ) {
 	return;
+}
 
 // Increase loop count
 $woocommerce_loop['loop']++;
 
 // Extra post classes
 $classes = array();
-$classes[] = anaglyph_woo_get_column_class();
 if ( 0 == ( $woocommerce_loop['loop'] - 1 ) % $woocommerce_loop['columns'] || 1 == $woocommerce_loop['columns'] ) {
-	$time_post_delay = 0;
 	$classes[] = 'first';
 }
-if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] )
+if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] ) {
 	$classes[] = 'last';
-if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] )
-	$classes[] = 'last';
-
-	$animation_class 	= esc_attr($anaglyph_config['shop-animations']); 
-	$delay_interval 	= anaglyph_get_delay_interval($time_post_delay);
-	if (!is_single() && ($animation_class != 'none')) {
-		$animation_class = 'data-scroll-reveal="enter '.$animation_class.' and move 50px '.$delay_interval.'"';
-	} else {
-		$animation_class = '';
-	}
-
+}
 ?>
-<li <?php post_class( $classes ); ?> <?php echo $animation_class; ?>>
+<li <?php post_class( $classes ); ?>>
 
 	<?php do_action( 'woocommerce_before_shop_loop_item' ); ?>
 
@@ -63,11 +56,14 @@ if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] )
 			 * @hooked woocommerce_template_loop_product_thumbnail - 10
 			 */
 			do_action( 'woocommerce_before_shop_loop_item_title' );
-		?>
 
-		<h2><?php the_title(); ?></h2>
+			/**
+			 * woocommerce_shop_loop_item_title hook
+			 *
+			 * @hooked woocommerce_template_loop_product_title - 10
+			 */
+			do_action( 'woocommerce_shop_loop_item_title' );
 
-		<?php
 			/**
 			 * woocommerce_after_shop_loop_item_title hook
 			 *
@@ -79,6 +75,15 @@ if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] )
 
 	</a>
 
-	<?php do_action( 'woocommerce_after_shop_loop_item' ); ?>
+	<?php
+
+		/**
+		 * woocommerce_after_shop_loop_item hook
+		 *
+		 * @hooked woocommerce_template_loop_add_to_cart - 10
+		 */
+		do_action( 'woocommerce_after_shop_loop_item' );
+
+	?>
 
 </li>
--- woocommerce/content-product_cat.php
+++ woocommerce/content-product_cat.php
@@ -9,29 +9,26 @@
  * @version     2.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 global $woocommerce_loop;
 
-$classes[] = anaglyph_woo_get_column_class();
 // Store loop count we're currently on
-if ( empty( $woocommerce_loop['loop'] ) )
+if ( empty( $woocommerce_loop['loop'] ) ) {
 	$woocommerce_loop['loop'] = 0;
+}
 
 // Store column count for displaying the grid
-if ( empty( $woocommerce_loop['columns'] ) )
+if ( empty( $woocommerce_loop['columns'] ) ) {
 	$woocommerce_loop['columns'] = apply_filters( 'loop_shop_columns', 4 );
-	
-// Increase loop count
-$woocommerce_loop['loop']++;
-?>
-<li class="product-category product<?php
-    if ( ( $woocommerce_loop['loop'] - 1 ) % $woocommerce_loop['columns'] == 0 || $woocommerce_loop['columns'] == 1 )
-        echo ' first';
-	if ( $woocommerce_loop['loop'] % $woocommerce_loop['columns'] == 0 )
-		echo ' last';
-	?>">
+}
 
+// Increase loop count
+$woocommerce_loop['loop'] ++;
+?>
+<li <?php wc_product_cat_class(); ?>>
 	<?php do_action( 'woocommerce_before_subcategory', $category ); ?>
 
 	<a href="<?php echo get_term_link( $category->slug, 'product_cat' ); ?>">
@@ -45,14 +42,14 @@ $woocommerce_loop['loop']++;
 			do_action( 'woocommerce_before_subcategory_title', $category );
 		?>
 
-		<h2>
+		<h3>
 			<?php
 				echo $category->name;
 
 				if ( $category->count > 0 )
 					echo apply_filters( 'woocommerce_subcategory_count_html', ' <mark class="count">(' . $category->count . ')</mark>', $category );
 			?>
-		</h2>
+		</h3>
 
 		<?php
 			/**
@@ -64,5 +61,4 @@ $woocommerce_loop['loop']++;
 	</a>
 
 	<?php do_action( 'woocommerce_after_subcategory', $category ); ?>
-
 </li>
