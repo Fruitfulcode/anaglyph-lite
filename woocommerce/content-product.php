fe64e77d8b63e9ef24bf4753df8efb15cd972903
staciedev <a.borenko@gmail.com>
2016-07-11 10:19:43 +0300

    WooCommerce columns fixed

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -15,32 +15,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $product, $woocommerce_loop;
 
-// Store loop count we're currently on
-if ( empty( $woocommerce_loop['loop'] ) ) {
-	$woocommerce_loop['loop'] = 0;
-}
-
-// Store column count for displaying the grid
-if ( empty( $woocommerce_loop['columns'] ) ) {
-	$woocommerce_loop['columns'] = apply_filters( 'loop_shop_columns', 4 );
-}
-
 // Ensure visibility
 if ( ! $product || ! $product->is_visible() ) {
 	return;
 }
 
-// Increase loop count
-$woocommerce_loop['loop']++;
-
 // Extra post classes
 $classes = array();
-if ( 0 == ( $woocommerce_loop['loop'] - 1 ) % $woocommerce_loop['columns'] || 1 == $woocommerce_loop['columns'] ) {
-	$classes[] = 'first';
-}
-if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] ) {
-	$classes[] = 'last';
-}
+
 
 $classes[] = anaglyph_woo_get_column_class();
 
